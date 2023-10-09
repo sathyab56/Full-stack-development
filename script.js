ce55a8063771a9ea1864f6510908f7ac1618616e
sathyab56 <147052015+sathyab56@users.noreply.github.com>
2023-10-09 10:16:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const submit = document.getElementById('submitBtn');
+
+
+submit.addEventListener('click',() => {
+
+     event.preventDefault();
+     
+    const userName=document.getElementById("userName").Value;
+    const userGmail=document.getElementById("userGmail").value;
+    const userNumber=document.getElementById("userNumber").value;
+    const userAddress=document.getElementById("userAddress").value;
+
+    console.log('name:$ {userName}'
+    console.log('Email: ${userEmail}'
+    console.log('Number: ${userNumber}'
+    console.log('Address: ${userAddress}'
+    
+});
